5656ea0b99b7a0f2e0ecc1e0e774c37a92cab833
Stud. Michele Farchetto <m.farchetto.0767@vallauri.net>
2020-10-02 13:04:42 +0200

    modifica html e rnd angular

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  names = ["BOND, JAMES BOND","BATMAN","TERMINATOR","CHARLES LECLERC","DARTH VADER","LUKE SKYWALKER"];
+  myName = 'Michele Farchetto'
+
+  changeName(){
+    this.myName = this.myName[this.getRandomInt(1,6)];
+    //this.names[Math.floor((Math.random() * this.names.length))] è l'alternatian javascript
+  }
+
+  getRandomInt(min,max){
+    return Math.floor(Math.random() * (max-min+1))+min;
+  }
 }
