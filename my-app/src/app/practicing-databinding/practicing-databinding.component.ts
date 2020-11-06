74ce7bb342767595bf144a934031b6fed8a9c1f5
Michele-Farchetto <71264401+Michele-Farchetto@users.noreply.github.com>
2020-11-06 11:29:15 +0100

    correzione compito angular, vedi if condensato

--- my-app/src/app/practicing-databinding/practicing-databinding.component.ts
+++ my-app/src/app/practicing-databinding/practicing-databinding.component.ts
@@ -17,16 +17,15 @@ export class PracticingDatabindingComponent implements OnInit {
 
   onResetUsername(){
     this.username = '';
+    this.allowEnabledButton = false;
   }
 
   onUpdateUsername(event: any){
-    this.username = event.target.value;
-    if (this.username != '') {
-      this.allowEnabledButton = true;
-    }
-    else{
-      this.allowEnabledButton = false;
-    }
+    //this.username = event.target.value;
+    //con mgmodel , non è necessario usare la stringa sopra
+    //perchè il comando ngmodel ti permette di non doversi preoccupare della variabile perchè la prende in automatico
+    this.allowEnabledButton = this.username != '';
+    //questa strnga serve per fare un il condensato , in modo da settare a true , nel caso si verifichi la conizione seguente
   }
 
 }
